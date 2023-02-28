cec7b704347067d3f374cb32d8c324da2c9c05e1
Phil McGarty <philmcgarty@hotmail.com>
2023-02-28 14:09:58 -0500

    api call added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log('Hello!');
+var cityName = "Ottawa";
+var lat = 45.424721;
+var lon = -75.6972;
+// var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=metric&appid=8afb55e108bfe22e6df569f88292df63"
+
+// function to pull API data
+var getWeatherData = function(lat, lon) {
+    // format the github api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=metric&appid=8afb55e108bfe22e6df569f88292df63";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
+
+getWeatherData(lat, lon);
+
+
+// function on load to check for previous saved data
+// if data = null then just load searchbars
+// else load previous search and data
+
+
+
+// function to generate html elements based on API data
+
+// event listener for search
